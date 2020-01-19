dff6d5b047a6ad17d81291af7b37e390d19ad692
pelop <pelop@RayDark>
2020-01-19 12:39:10 +0100

    Creamos el test de pruebas

---
+++ test/depositoCombustible/DepositoCombustible.java
@@ -0,0 +1,39 @@
+package depositoCombustible;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DepositoCombustible {
+
+	@Test
+	public void testGetDepositoNivel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDepositoMax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstaVacio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstaLleno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFill() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConsumir() {
+		fail("Not yet implemented");
+	}
+
+}
